2e0484a07c956351796b281469598219bcd9bc23
Boniface <113481026+onoyima@users.noreply.github.com>
2024-08-08 11:27:32 +0100

    Update 8-index_my_names.sql

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,2 +1,2 @@
--- Task 08: Create an index on the first letter of name
-CREATE INDEX idx_name_first ON names (LEFT(name, 1));
+-- Task 8: Create index on the first letter of name
+CREATE INDEX idx_name_first ON names (name(1));
