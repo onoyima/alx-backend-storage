0a5144f3bdbee6d48cb773fd57f419fdd0c66fc9
Boniface <113481026+onoyima@users.noreply.github.com>
2024-08-08 11:28:04 +0100

    Update 9-index_name_score.sql

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,2 +1,2 @@
--- Task 09: Create an index on the first letter of name and score
-CREATE INDEX idx_name_first_score ON names (LEFT(name, 1), score);
+-- Task 9: Create index on the first letter of name and score
+CREATE INDEX idx_name_first_score ON names (name(1), score);
