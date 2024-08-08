88273622dcd58593a69a4ebada0a6f579e753570
Boniface <113481026+onoyima@users.noreply.github.com>
2024-08-08 11:20:08 +0100

    Update 2-fans.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,6 @@
--- Task 2: Rank country origins of bands by number of fans
-SELECT origin, nb_fans
-FROM bands
+-- 2-fans.sql: Rank countries by number of fans from metal_bands table
+
+SELECT origin, SUM(fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
 ORDER BY nb_fans DESC;
