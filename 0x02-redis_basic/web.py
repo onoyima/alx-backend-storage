58c3b60d48e78f3cc3a20938bff07778e0b1707f
onoyima <clintonfaze@outlook.com>
2024-08-14 22:28:14 +0000

    0x02-redis_basic project

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -33,6 +33,6 @@ def data_cacher(method: Callable) -> Callable:
 @data_cacher
 def get_page(url: str) -> str:
     '''Returns the content of a URL after caching the request's response,
-    and tracking the requests.
+    and tracking the request.
     '''
     return requests.get(url).text
